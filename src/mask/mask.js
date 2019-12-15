b1670ef0cf1610afd4bc91fd89883a7308f86814
tobyDing <17051037502@sina.cn>
2019-12-15 20:28:08 +0800

    增加mask组件;

---
+++ src/mask/mask.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {
+    extClass: {
+      type: String,
+      value: ''
+    },
+    visible: {
+      type: Boolean,
+      value: true
+    },
+    transparent: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {},
+  methods: {}
+})
