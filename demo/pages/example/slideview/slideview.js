7f28326ae0dc39413c09b33804623fe74e0d3590
丁天成 <dingtiancheng@kuaidihelp.com>
2019-12-17 18:44:00 +0800

    完成slideview组件;

--- demo/pages/example/slideview/slideview.js
+++ demo/pages/example/slideview/slideview.js
@@ -1,66 +1,30 @@
-// pages/example/slideview/slideview.js
+import MsDialog from '../../../components/dialog/index';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onTapSlideview2(ev) {
+    let slideview = this.selectComponent('#ms-slideview2');
+    MsDialog.alert({
+      content: '确定要删除吗？',
+      onButtonsTap: (ev) => {
+        const { index } = ev
+        if (index == 1) {
+          slideview.close()
+          MsDialog.close()
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onTapSlideview3(ev) {
+    let slideview = this.selectComponent('#ms-slideview3');
+    MsDialog.alert({
+      content: '确定要关闭吗？',
+      onButtonsTap: (ev) => {
+        const { index } = ev
+        if (index == 1) {
+          slideview.close()
+          MsDialog.close()
+        }
+      }
+    })
   }
 })
--- src/slideview/slideview.js
+++ src/slideview/slideview.js
@@ -1,3 +1,5 @@
+const threshold = 0.3;
+
 Component({
   options: {
     multipleSlots: true,
@@ -8,8 +10,90 @@ Component({
       type: String,
       value: ''
     },
+    leftWidth: {
+      type: Number,
+      value: 0
+    },
+    rightWidth: {
+      type: Number,
+      value: 0
+    }
   },
-  data: {},
-  ready() {},
-  methods: {}
+  data: {
+    slideStyle: ''
+  },
+  methods: {
+    onDragStart(ev) {
+      const { touches } = ev
+      const touch = touches[0]
+      const { clientX, clientY } = touch
+      this.clientX = clientX
+      this.clientY = clientY
+      this.offsetX = 0
+      this.offsetY = 0
+      this.direction = ''
+    },
+    onDragMove(ev) {
+      const { touches } = ev
+      const touch = touches[0]
+      const { clientX, clientY } = touch
+      this.offsetX = clientX - this.clientX
+      this.offsetY = clientY - this.clientY
+      // 确定滑动方向
+      if (Math.abs(this.offsetX) > Math.abs(this.offsetY)) {
+        this.direction = 'horizontal'
+      } else {
+        this.direction = ''
+      }
+    },
+    onDragEnd() {
+      if (this.offsetX == 0) {
+        return;
+      }
+      if (this.direction === 'horizontal') {
+        this.slideviewMove(this.offsetX)
+      }
+    },
+    slideviewMove(offset) {
+      /**
+       * offset 
+       * =0 关闭视图
+       * <0 向左滑动 显示右侧内容
+       * >0 向右滑动 显示左侧内容
+       */
+      const { leftWidth, rightWidth } = this.data;
+      let realOffset = 0
+      let asbOffset = Math.abs(offset)
+      // 去除无效滑动
+      if ((offset < 0 && asbOffset < rightWidth * threshold) || (offset > 0 && asbOffset < leftWidth * threshold)) {
+        console.log('阻止了')
+        return
+      }
+      // 左滑
+      if (offset < 0 && asbOffset > rightWidth * threshold) {
+        realOffset = this.openDriction == 'left' ? 0 : -rightWidth
+      }
+      // 右滑
+      if (offset > 0 && asbOffset > leftWidth * threshold) {
+        realOffset = this.openDriction == 'right' ? 0 : leftWidth
+      }
+      // 获取滑动视图开启状态
+      if (realOffset == 0) {
+        this.openDriction = ''
+      } else {
+        this.openDriction = realOffset < 0 ? 'right' : 'left'
+      }
+      // 实施滑动
+      let slideStyle = `
+        transform: translate3d(${realOffset}rpx, 0, 0);
+        -webkit-transform: translate3d(${realOffset}rpx, 0, 0);
+      `
+      this.setData({
+        slideStyle
+      })
+    },
+    close(){
+      this.slideviewMove(0)
+    }
+  }
 })
