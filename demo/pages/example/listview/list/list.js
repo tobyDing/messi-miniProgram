07b3084ac6e8fdb427ea87cb5d7d14ab7758bef4
丁天成 <dingtiancheng@kuaidihelp.com>
2019-12-19 18:41:28 +0800

    listview

--- demo/pages/example/listview/list/list.js
+++ demo/pages/example/listview/list/list.js
@@ -1,3 +1,61 @@
-Page({
 
+const MaxConut = 3
+const PageNum = 10
+Page({
+  data: {
+    userData: {
+      page: 1,
+      list: []
+    }
+  },
+  onLoad() {
+    this.loadData().then((res) => {
+      this.setData({
+        userData: res
+      })
+    })
+  },
+  loadData: function (page = 1) {
+    console.log('page', page)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        let res = {}
+        let list = []
+        if (page <= MaxConut) {
+          list = Array.from({ length: PageNum }, (item, index) => {
+            return {
+              "id": `${page}-${index + 1}`,
+              "name": `${page}-小刘${index + 1}`,
+              "mobile": "15777420459",
+              "brand": "申通快递",
+              "address": `河北省邢台市清河县美丽花园${index + 1}号`,
+            }
+          })
+          res = {
+            page,
+            count: MaxConut,
+            list
+          }
+        } else {
+          res = {
+            page,
+            count: MaxConut,
+            list: []
+          }
+        }
+        resolve(res)
+      }, 1000)
+    })
+  },
+  onEmitDown() {
+    let { userData: { page, list } } = this.data;
+    this.loadData(++page).then((res) => {
+      if (res.list.length >= PageNum) {
+        res.list = list.concat(res.list)
+        this.setData({
+          userData: res
+        })
+      }
+    })
+  }
 })
--- src/listview/listview.js
+++ src/listview/listview.js
@@ -1,14 +1,27 @@
 Component({
   options: {
-    multipleSlots: true,
     addGlobalClass: true
   },
   properties: {
-    extClass: {
+    mode: {//列表模式 up下拉刷新;down上拉加载;all全部;
       type: String,
-      value: ''
+      value: 'all'
+    },
+    lowerThreshold: {//距离底部多远，触发上拉加载事件
+      type: Number,
+      value: 50
     },
   },
-  data: {},
-  methods: {}
+  data: {
+    downstatus: '',//'loading'加载中;'nodata'无内容;
+  },
+  methods: {
+    onEmitDown(ev) {
+      /**
+       * 上拉加载事件
+       */
+      // console.log('onEmitDown.ev', ev)
+      // this.triggerEvent('onEmitDown')
+    }
+  }
 })
