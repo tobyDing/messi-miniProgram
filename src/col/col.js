4f129dbffa0b608f92701cbeaf35d92286b3a13b
tobyding <tianchengding1314@163.com>
2020-05-23 15:55:48 +0800

    增加弹性布局组件;

--- src/col/col.js
+++ src/col/col.js
@@ -4,7 +4,33 @@ Component({
     addGlobalClass: true
   },
   externalClasses: ['custom-class'],
-  properties: {},
-  data: {},
-  methods: {}
+  relations: {
+    '../row/row': {
+      type: 'ancestor'
+    }
+  },
+  properties: {
+    span: {
+      type: String,
+      value: ''
+    },
+    offset: {
+      type: String,
+      value: ''
+    }
+  },
+  data: {
+    style: ''
+  },
+  methods: {
+    setGutter(gutter) {
+      gutter = gutter * 1
+      if (gutter) {
+        let style = `padding-left:${gutter / 2}rpx;padding-right:${gutter / 2}rpx;`;
+        this.setData({
+          style
+        })
+      }
+    }
+  }
 })
--- src/row/row.js
+++ src/row/row.js
@@ -1,10 +1,38 @@
+const ColKey = '../col/col'
+
 Component({
   options: {
     multipleSlots: true,
     addGlobalClass: true
   },
   externalClasses: ['custom-class'],
-  properties: {},
+  relations: {
+    [ColKey]: {
+      type: 'descendant',
+      linked(target) {
+        target && target.setGutter && target.setGutter(this.data.gutter)
+      }
+    }
+  },
+  properties: {
+    gutter: {
+      type: String,
+      value: '',
+      observer: 'setGutter'
+    }
+  },
   data: {},
-  methods: {}
+  ready(){
+    this.setGutter()
+  },
+  methods: {
+    setGutter() {
+      const { gutter } = this.data;
+      if (gutter) {
+        this.getRelationNodes(ColKey).forEach(target => {
+          target && target.setGutter && target.setGutter(gutter)
+        });
+      }
+    }
+  }
 })
