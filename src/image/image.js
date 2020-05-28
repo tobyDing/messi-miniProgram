39f18672ab69a842d7f304b4445ed152ce5d5bc1
tobyding <tianchengding1314@163.com>
2020-05-28 10:32:29 +0800

    增加image组件;

--- src/image/image.js
+++ src/image/image.js
@@ -1,10 +1,73 @@
+
 Component({
-  options: {
-    multipleSlots: true,
-    addGlobalClass: true
+  externalClasses: ['custom-class', 'image-class'],
+  properties: {
+    src: {
+      type: String,
+      value: '',
+      observer(newVal, oldVal) {
+        newVal && this.setData({
+          error: false,
+          loading: true
+        });
+      }
+    },
+    width: {
+      type: String,
+      value: ''
+    },
+    height: {
+      type: String,
+      value: ''
+    },
+    mode: {
+      type: String,
+      value: 'scaleToFill'
+    },
+    layout: {
+      type: String,
+      value: 'defalut'
+    },
+    round: {
+      type: Boolean,
+      value: false
+    },
+    lazyLoad: {
+      type: Boolean,
+      value: false
+    },
+    showLoading: {
+      type: Boolean,
+      value: false
+    },
+    showError: {
+      type: Boolean,
+      value: true
+    },
+    customStyle: {
+      type: String,
+      value: ''
+    }
   },
-  externalClasses: ['custom-class'],
-  properties: {},
-  data: {},
-  methods: {}
+
+  data: {
+    loading: false,
+    error: false,
+  },
+
+  methods: {
+    onImgLoad(ev) {
+      this.setData({
+        loading: false
+      })
+      this.triggerEvent('load', ev.detail)
+    },
+    onImgError(ev) {
+      this.setData({
+        loading: false,
+        error: 'error'
+      })
+      this.triggerEvent('error', ev.detail)
+    }
+  }
 })
